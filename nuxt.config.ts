072687fa2b74087b3bc12bc307b794c7bb07143e
i-yuka1129 <y.izumisawa1129@gmail.com>
2023-12-13 16:09:01 +0900

    add pages,store,middlware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    
+  <div class="container flex items-center justify-cneter">
+    <h1>Welcome</h1>
+    <p>Sing in tu your profile</p>
+    <button class="px-3 py-2 rounded-md bg-emerald-500 text-white inline-flex items-center">Sing In</button>
   </div>
 </template>
 
 <script lang="ts" setup>
+  const userStore = useUserStore();
 
+  const signIn =async () => {
+    await userStore.signIn({
+      username: 'kminchelle',
+      password: '0lelplR',
+    });
+
+    await navigateTo('/profile',)
+  }
 </script>
 
 <style>
